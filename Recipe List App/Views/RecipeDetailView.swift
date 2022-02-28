ad06361fa9894eafe88ed107575e15d0d7d85381
Marcelo Aguiar <marcelo.aguiar@gmail.com>
2022-02-28 07:00:27 -0800

    Add new json data Modify json parsing Modify detail view to display ingredient name

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -26,9 +26,9 @@ struct RecipeDetailView: View {
                         .font(.headline)
                         .padding([.bottom, .top], 5)
                     
-                    ForEach (recipe.ingredients, id:\.self) { item in
+                    ForEach (recipe.ingredients) { item in
                         
-                        Text("- " + item)
+                        Text("- " + item.name)
                     }
                 }
                 .padding(.horizontal)
